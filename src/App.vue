e3d11c757eaced88b5994a4d5a4e165e9c95282f
Pavel Zavezenov <pavel290430@yandex.ru>
2023-09-02 23:48:48 +0700

    day cards

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script setup>
+  import Weather from './components/Weather.vue';
+</script>
+
+<template>
+  <header class="header">
+    <section class="search" aria-label="Выбор города">
+      <form class="city-form">
+        <img class="city-form__search-icon city-form__search-icon_gray" alt="Поиск" src="./assets/search-icon.svg"/>
+        <input class="city-form__input" placeholder="Город" />
+        <button class="city-form__submit" type="submit">
+          <img class="city-form__search-icon_white" src="./assets/search-icon_white.svg" alt="Поиск"/>
+        </button>
+      </form>
+    </section>
+  </header>
+  <main class="main">
+    <Weather/>
+  </main>
+</template>
+
+<style scoped>
+
+  .header {
+    display: flex;
+    justify-content: center;
+  }
+  .search {
+    display: flex;
+    margin-top: 50px;
+    background-color: #ECEEF2;
+    border-radius: 9px;
+    height: 72px;
+    align-items: center;
+    justify-content: center;
+    width: 40%;
+  }
+
+  .city-form {
+    display: flex;
+    align-items: center;
+    position: relative;
+    width: 100%;
+  }
+
+  .city-form__search-icon_gray {
+    margin-left: 25px;
+  }
+
+  .city-form__submit {
+    width: 95px;
+    padding: 0;
+    margin: 0 25px 0 10px;
+    height: 34px;
+    border-radius: 48px;
+    background-color: black;
+    cursor: pointer;
+  }
+
+  .city-form__input {
+    background-color: transparent;
+    border: none;
+    color: #A0A0A0;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 20px;
+    margin-left: 16px;
+    outline: none;
+    width: 100%;
+  }
+
+  .city-form__input::placeholder {
+    color: #A0A0A0;
+  }
+
+  .main {
+    display: flex;
+    justify-content: center;
+  }
+
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,72 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const days = ref(0);
+    const city = ref('Здесь будет ваш город');
+    
+    function setDays(n) {
+        days.value = n;
+    }
+</script>
+
+<template>
+    <section class="weather" aria-label="Погода">
+        <div class="weather__city-wrapper">
+            <h1 class="weather__city">{{city}}</h1>
+            <button 
+                v-on:click=setDays(5) 
+                type="button" 
+                class="weather__5days weather__days-button" 
+                v-bind:class="[days === 5 ? 'active' : '']">
+                5 дней
+            </button>
+            <button 
+                v-on:click=setDays(10) 
+                type="button" 
+                class="weather__10days weather__days-button" 
+                v-bind:class="[days === 10 ? 'active' : '']">
+                10 дней
+            </button>
+        </div>
+        
+    </section>
+</template>
+
+<style scoped>
+
+    .weather {
+        width: 40%;
+    }
+
+    .weather__city-wrapper {
+        display: flex;
+        align-items: center;
+    }
+
+    .weather__city {
+        margin-right: auto;
+    }
+
+    .weather__days-button {
+        background-color: #ECEEF2;
+        border: transparent;
+        border-radius: 20px;
+        height: 40px;
+        width: 80px;
+        cursor: pointer;
+        transition: all .5s ease-in-out;
+    }
+    .weather__days-button:hover {
+        border: 1px solid black;
+    }
+
+    .weather__days-button:first-of-type {
+        margin-right: 30px;
+    }
+
+    .active {
+        color: white;
+        background-color: black;
+    }
+
+</style>
